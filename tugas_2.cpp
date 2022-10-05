7ba1c031b7fd2ffe8a3bf31f84e68884be792a7d
Nadindra MA <107752721+NMATech@users.noreply.github.com>
2022-10-05 13:30:21 +0700

    Nadindra Tugas pertemuan pertama

---
+++ tugas_2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //menggunakan variable sementara
+
+    //nilai variable
+    int x;
+    x = 7;
+
+    //cout pertama sebelum nilai diganti atau ditimpa
+    cout << x << endl;
+
+    //saya coba ganti nilai dari x
+    x = 11;
+
+    //tampilkan cout setelah nilai diganti
+    cout << x << endl;
+
+    return 0;
+}
---
+++ tugas_2b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //tanpa menggunakan variable sementara
+
+    //nilai variable
+    int y = 5;
+
+    //cout
+    cout << y*2 << endl;
+
+    return 0;
+}
